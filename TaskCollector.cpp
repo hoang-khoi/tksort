5a627d168244d2a8ae5a991826a3e1651d86ca54
Khoi Hoang <khoi_hoang@hotmail.com>
2020-05-02 19:16:27 +0900

    Filtered out empty lines

--- TaskCollector.cpp
+++ TaskCollector.cpp
@@ -5,8 +5,12 @@ std::vector<TaskNode> TaskCollector::collect(std::istream &in) {
     std::vector<TaskNode> taskNodes;
     std::string buf;
 
-    while (std::getline(in, buf))
+    while (std::getline(in, buf)) {
+        if (buf.empty())
+            continue;
+
         taskNodes.emplace_back(buf);
+    }
 
     return taskNodes;
 }
